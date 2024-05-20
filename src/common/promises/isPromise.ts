9f1fde5c16f41a3129d5ce804d07b2aa5bde2109
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-20 15:27:43 +0300

    squash!

--- src/common/promises/isPromise.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Microsoft Corporation.
- *
- * Licensed under the MIT license found in the
- * LICENSE file in the root of this projects source tree.
- */
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const isPromise: (value: unknown) => boolean = (value: any) => value != null && typeof value.then === "function";
